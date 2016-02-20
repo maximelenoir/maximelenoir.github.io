5dd61139db4dba017bf911b5f34efeb35157090f
Maxime Lenoir <lenoir.maxime0@gmail.com>
2016-02-20 22:07:31 +0100

    Update hourglas for 0.6.3

--- hourglass/implementors/core/iter/trait.Iterator.js
+++ hourglass/implementors/core/iter/trait.Iterator.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['libc'] = [];implementors['hourglass'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/iter/trait.Iterator.html' title='core::iter::Iterator'>Iterator</a> for <a class='struct' href='hourglass/struct.Every.html' title='hourglass::Every'>Every</a>",];
+implementors['libc'] = [];implementors['hourglass'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/iter/trait.Iterator.html' title='core::iter::Iterator'>Iterator</a> for <a class='struct' href='hourglass/struct.Every.html' title='hourglass::Every'>Every</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/iter/trait.Iterator.html' title='core::iter::Iterator'>Iterator</a> for <a class='struct' href='hourglass/struct.Range.html' title='hourglass::Range'>Range</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
